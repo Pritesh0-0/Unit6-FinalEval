5f2ed8b4c86346fa0bf7d1595d4a006043de4401
Pritesh0_0 <patilpritesh347@gmail.com>
2022-08-06 17:51:41 +0530

    commit

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
  import "./HomePage.css"
  import { useNavigate} from "react-router-dom";
- import './cart.css'
+
 
  const Cart = () => {
  const [data,setData]=useState([])
